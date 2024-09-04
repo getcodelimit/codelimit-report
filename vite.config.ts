04b4534680316dec2998e3332bbc9c6f733fde42
Rob van der Leek <5324924+robvanderleek@users.noreply.github.com>
2024-09-04 22:00:39 +0200

    Single output file

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { viteSingleFile } from 'vite-plugin-singlefile'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), viteSingleFile()],
   server: {
     port: 3000,
     open: true
